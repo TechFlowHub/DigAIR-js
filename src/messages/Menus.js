81336633fc2facc1c98eccc01d1bebefa8d6c7be
viniimiguel <viniciusmiguelgit2@gmail.com>
2025-03-15 14:46:49 -0300

    menus

---
+++ src/messages/Menus.js
@@ -0,0 +1,10 @@
+exports.FIRST_MESSAGE = `
+🟢 Olá! Seja bem-vindo(a) ao Assistente Virtual do Imposto de Renda! 📊💰\n",
+"Eu estou aqui para tirar suas dúvidas sobre a declaração e te ajudar a evitar problemas com a Receita Federal! 🚀💡 Como posso ajudar você hoje?\n",
+"Pergunte o que quiser e eu te responderei rapidinho. Vamos começar? 😃👇\n"
+`
+
+exports.INVALID_MESSAGE = `
+"⚠️ *Opção inválida!* Por favor, digite um número de *1 a 9* para escolher uma pergunta ou *0 para encerrar* o atendimento."
+`
+        
