855006434d3a2fd74ec9006575ac85f77b221ba2
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2025-04-25 13:23:25 -0300

    feat: add MySQL backup script for automated database backups

---
+++ script/mysql_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONTAINER_NAME="mysql-container"
+DB_NAME="chatbot_favip"
+DB_USER="root"
+DB_PASS="sua_senha"
+BACKUP_DIR="/root/backups"
+
+DATE=$(date +%F)
+FILENAME="${DB_NAME}_${DATE}.sql.gz"
+
+mkdir -p "$BACKUP_DIR"
+
+docker exec "$CONTAINER_NAME" mysqldump -u "$DB_USER" -p"$DB_PASS" --single-transaction "$DB_NAME" | gzip > "$BACKUP_DIR/$FILENAME"
